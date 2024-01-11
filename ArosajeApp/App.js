2786bf0e261eba72498fb0093fef71179fe632c2
Yuubitsume <jeannicolas34980@gmail.com>
2024-01-11 12:28:07 +0100

    add image on tabBar, Home + add connexion + inscription

--- ArosajeApp/App.js
+++ ArosajeApp/App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+import { Home, Leaf, CircleUser, MessagesSquare } from 'lucide-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from './screens/HomeScreen/HomeScreen';
 import { PlantScreen } from './screens/PlantScreen/PlantScreen';
 import { ProfileScreen } from './screens/ProfileScreen/ProfileScreen';
+import { ChatScreen } from './screens/ChatScreen/ChatScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,9 +14,54 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Plant" component={PlantScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Home color="black" size={30} />
+            ),
+            headerShown: false, // Masquer l'en-tête pour cet écran
+          }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MessagesSquare color="black" size={30} />
+            ),
+            headerShown: false, // Masquer l'en-tête pour cet écran
+          }}
+          listeners={({ navigation }) => ({
+            tabPress: (e) => {
+              // Prevent the default action
+              e.preventDefault();
+              // Navigate to "OnBoardingScreen" when the tab is pressed
+              navigation.navigate('OnBoardingScreen');
+            },
+          })}
+        />
+        <Tab.Screen
+          name="Plant"
+          component={PlantScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Leaf color="black" size={30} />
+            ),
+            headerShown: false, // Masquer l'en-tête pour cet écran
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <CircleUser color="black" size={30} />
+            ),
+            headerShown: false, // Masquer l'en-tête pour cet écran
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- ArosajeApp/screens/HomeScreen/HomeScreen.jsx
+++ ArosajeApp/screens/HomeScreen/HomeScreen.jsx
@@ -1,19 +1,59 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 export function HomeScreen() {
+  const handleConnecter = () => {
+    // Action à effectuer lorsque le bouton "Se Connecter" est pressé
+    console.log("Bouton 'Se Connecter' pressé !");
+  };
+
+  const handleInscription = () => {
+    // Action à effectuer lorsque le bouton "S'inscrire" est pressé
+    console.log("Bouton 'S'inscrire' pressé !");
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>C'est la maison ou bien</Text>
-    </View>
+    <ImageBackground
+      source={require('../../assets/Home/homePicture.png')} // Remplacez par le chemin correct de votre image
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Arosa'je</Text>
+        <TouchableOpacity style={styles.button} onPress={handleConnecter}>
+          <Text style={styles.buttonText}>Se Connecter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleInscription}>
+          <Text style={styles.buttonText}>S'inscrire</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+    color: '#fff',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#000000',
+    fontSize: 16,
   },
 });
