6f9a9e1784d90242740c5cd264368fd48e10e8d4
Florian <flo.guiot@hotmail.fr>
2024-02-29 09:12:41 +0100

    Test requete pre-vol

--- ArosajeDash/pages/api/plantes/add.js
+++ ArosajeDash/pages/api/plantes/add.js
@@ -7,6 +7,11 @@ const prisma = new PrismaClient();
 
 
 export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    // Répondre favorablement aux requêtes OPTIONS pré-vol
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).end(); // Méthode non autorisée
   }
