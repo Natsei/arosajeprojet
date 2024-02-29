f66ad815be19909bcfda0e808021ab8d44494959
Yuubitsume <jeannicolas34980@gmail.com>
2024-02-29 09:50:52 +0100

    Change gitignore

--- ArosajeApp/screens/AddPictureScreen/AddPictureScreen.jsx
+++ ArosajeApp/screens/AddPictureScreen/AddPictureScreen.jsx
@@ -1,68 +1,117 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput, Modal } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 import ImagePicker from 'react-native-image-picker';
-import RNPickerSelect from 'react-native-picker-select';
 
 const AddPictureScreen = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [description, setDescription] = useState('');
-  const [selectedValue, setSelectedValue] = useState('plante'); // État pour stocker la plante sélectionnée
+  const [selectedValue, setSelectedValue] = useState('plante');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
 
   const handleImagePick = () => {
-    const options = {
-      title: 'Choisir une image',
-      cancelButtonTitle: 'Annuler',
-      takePhotoButtonTitle: 'Prendre une photo',
-      chooseFromLibraryButtonTitle: 'Choisir depuis la galerie',
-      mediaType: 'photo',
-      storageOptions: {
-        skipBackup: true,
-      },
-    };
-
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log("L'utilisateur a annulé la sélection de l'image");
-      } else if (response.error) {
-        console.log("Erreur: ", response.error);
-      } else {
-        console.log("Image sélectionnée: ", response.uri);
-        setSelectedImage(response.uri);
-      }
-    });
+    try {
+      const options = {
+        title: 'Choisir une image',
+        cancelButtonTitle: 'Annuler',
+        takePhotoButtonTitle: 'Prendre une photo',
+        chooseFromLibraryButtonTitle: 'Choisir depuis la galerie',
+        mediaType: 'photo',
+        storageOptions: {
+          skipBackup: true,
+        },
+      };
+  
+      ImagePicker.launchImageLibrary(options, (response) => {
+        if (response.didCancel) {
+          console.log("L'utilisateur a annulé la sélection de l'image");
+        } else if (response.error) {
+          console.log("Erreur: ", response.error);
+        } else {
+          console.log("Image sélectionnée: ", response.uri);
+          setSelectedImage(response.uri);
+        }
+      });
+    } catch (error) {
+      console.error("Erreur lors de la sélection de l'image : ", error);
+    }
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        {selectedImage ? (
-          <Image source={{ uri: selectedImage }} style={styles.image} />
-        ) : (
-          <Text style={styles.placeholderText}>Glissez et déposez une image ici</Text>
-        )}
-      </View>
+  const handleOptionSelect = (option) => {
+    setSelectedValue(option.label);
+    setModalVisible(false);
+  };
 
-      <Text>Sélectionnez une option :</Text>
-      <RNPickerSelect
-  style={{
-    inputIOS: {
-      height: '10%',
-      width: '80%',
-      marginBottom: 20,
-      borderColor: 'gray',
-      borderWidth: 1,
-    },
-  }}
-  value={selectedValue}
-  onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-  items={[
+  const onCameraCapture = async (data) => {
+    try {
+      // Vous pouvez manipuler les données de l'image capturée ici, par exemple, l'enregistrer dans le stockage.
+      console.log("Image capturée: ", data.uri);
+      setSelectedImage(data.uri);
+    } catch (error) {
+      console.error("Erreur lors de la capture de l'image : ", error);
+    } finally {
+      setIsCameraOpen(false); // Fermer la caméra après la capture
+    }
+  };
+
+  const options = [
+    { label: 'Plante', value: 'plante' },
     { label: 'Java', value: 'java' },
     { label: 'JavaScript', value: 'js' },
     { label: 'Python', value: 'python' },
     { label: 'C#', value: 'csharp' },
-  ]}
-/>
-      <Text>Option sélectionnée : {selectedValue}</Text>
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View style={styles.dashedBorder}>
+          {selectedImage ? (
+            <Image source={{ uri: selectedImage }} style={styles.image} />
+          ) : (
+            <Text style={styles.text}>Glissez et déposez une image ici</Text>
+          )}
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => setIsCameraOpen(true)}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text>Sélectionnez une option :</Text>
+      <TouchableOpacity
+        style={styles.selectStyle}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.selectTextStyle}>{selectedValue}</Text>
+      </TouchableOpacity>
+
+      <Modal
+        transparent={true}
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {options.map((option, index) => (
+              <TouchableOpacity
+                key={index}
+                style={styles.modalOptionStyle}
+                onPress={() => handleOptionSelect(option)}
+              >
+                <Text style={styles.modalOptionTextStyle}>{option.label}</Text>
+              </TouchableOpacity>
+            ))}
+            <TouchableOpacity
+              style={styles.modalCancelStyle}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalOptionTextStyle}>Annuler</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
       <TextInput
         style={styles.descriptionInput}
@@ -71,16 +120,25 @@ const AddPictureScreen = () => {
         value={description}
       />
 
-      <Text style={styles.text}>Bienvenue sur l'application</Text>
+      {isCameraOpen && (
+        <RNCamera
+          style={styles.cameraPreview}
+          captureAudio={false}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {}}
+          onTextRecognized={({ textBlocks }) => {}}
+          onPictureTaken={(data) => onCameraCapture(data)}
+        />
+      )}
 
-      <TouchableOpacity style={styles.button} onPress={handleImagePick}>
-        <Text style={styles.buttonText}>Choisir une image</Text>
+      <TouchableOpacity style={styles.saveButton}>
+        <Text style={styles.buttonTextConfirm}>Enregistrer</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // Styles here
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -88,12 +146,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   imageContainer: {
-    width: '80%', 
-    height: '30%', 
+    position: 'relative',
+    width: '80%',
+    height: '30%',
     backgroundColor: '#F5F5F5',
-    justifyContent: 'center',
-    alignItems: 'center',
     marginBottom: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  dashedBorder: {
+    width: '100%',
+    height: '100%',
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderStyle: 'dashed',
+    position: 'absolute',
   },
   image: {
     width: '100%',
@@ -101,9 +169,49 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     borderRadius: 10,
   },
-  placeholderText: {
-    textAlign: 'center',
-    color: '#888',
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+    justifyContent: 'center',
+  },
+  selectStyle: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    width: '80%',
+  },
+  selectTextStyle: {
+    fontSize: 18,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderColor: 'rgba(0, 0, 0, 0.2)', // Effet de bordure transparente
+  },
+  modalOptionStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    paddingVertical: 15,
+  },
+  modalCancelStyle: {
+    backgroundColor: '#F2E8CF',
+    borderRadius: 10,
+    marginVertical: 10,
+    paddingVertical: 10, // Augmentez la taille du bouton
+  },
+  modalOptionTextStyle: {
+    fontSize: 18,
   },
   descriptionInput: {
     textAlign: 'left',
@@ -118,18 +226,39 @@ const styles = StyleSheet.create({
     width: '80%',
     paddingTop: 10,
   },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
+  saveButton: {
+    height: 50,
+    width: 150, 
+    backgroundColor: '#F2E8CF',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 30, 
+    paddingLeft: 2, 
+    marginBottom: 1, 
+  },
+  cameraPreview: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   button: {
+    position: 'absolute',
+    bottom: 40,
+    alignSelf: 'center',
+    height: 60,
+    width: 60,
     backgroundColor: '#F2E8CF',
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 10,
+    borderRadius: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
-    fontSize: 16,
+    color: 'black',
+    fontSize: 48,
+    fontWeight: 'bold',
+  },
+  buttonTextConfirm: {
+    fontSize: 26, 
     color: '#000000',
     textAlign: 'center',
   },
--- ArosajeApp/screens/DetailScreen/DetailScreen.jsx
+++ ArosajeApp/screens/DetailScreen/DetailScreen.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
 
-const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export function DetailScreen({ navigation }) {
 
@@ -44,7 +44,7 @@ export function DetailScreen({ navigation }) {
             source={require('../../assets/Profile/Image.png')}
             style={styles.smallImage}
           />
-          <View>
+          <View style={styles.userInfoContainer}>
             <Text style={styles.userInfo}>Michel Durand</Text>
             <Text style={styles.userInfoDescription}>Je suis Michel et j’aime les plantes</Text>
           </View>
@@ -67,89 +67,82 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFFFFF",
-    justifyContent: 'center',
-    padding: windowWidth * 0.05, // Ajouter un padding global à gauche
+    paddingHorizontal: windowWidth * 0.05,
+    paddingTop: windowHeight * 0.05,
   },
   title: {
-    fontSize: windowHeight * 0.04,
+    fontSize: windowWidth * 0.06,
     fontWeight: 'bold',
-    marginTop: 20,
-    textAlign: 'center', // Aligner le texte du titre à gauche
+    textAlign: 'center',
+    marginBottom: windowHeight * 0.05,
   },
   subtitle: {
-    fontSize: windowHeight * 0.025,
+    fontSize: windowWidth * 0.04,
     fontWeight: 'bold',
-    marginTop: windowHeight * 0.015,
-    textAlign: 'left', // Aligner le texte des sous-titres à gauche
+    marginTop: windowHeight * 0.03,
+    textAlign: 'left',
   },
   subtitleDescription: {
-    fontSize: 18,
-    marginTop: windowHeight * 0.015,
-    textAlign: 'left', // Aligner le texte des sous-titres à gauche
-  },
-  
-  content: {
-    alignItems: 'flex-start',
+    fontSize: windowWidth * 0.035,
     marginTop: windowHeight * 0.02,
+    textAlign: 'left',
+  },
+  content: {
+    alignItems: 'center',
   },
   image: {
-    width: 350,
-    height: 300,
+    width: windowWidth * 0.9,
+    height: windowHeight * 0.3,
     resizeMode: 'contain',
-    borderRadius: windowHeight * 0.02,
+    borderRadius: windowWidth * 0.02,
   },
   cityContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: windowHeight * 0.015,
+    marginTop: windowHeight * 0.02,
   },
   cityImage: {
-    width: 14,
-    height: 22,
+    width: windowWidth * 0.07,
+    height: windowHeight * 0.04,
     resizeMode: 'cover',
-    borderRadius: windowHeight * 0.01,
+    borderRadius: windowWidth * 0.01,
     marginRight: windowWidth * 0.02,
   },
   cityName: {
-    fontSize: windowHeight * 0.025, // Assurez-vous que la taille du texte est la même que pour les sous-titres
+    fontSize: windowWidth * 0.04,
     fontWeight: 'bold',
   },
   blackLine: {
     height: 0.5,
     width: '90%',
     backgroundColor: 'lightgrey',
-    marginBottom: windowHeight * 0.015, // Ajustez la marge en fonction de vos besoins
-    marginTop: windowHeight * 0.015,
+    marginVertical: windowHeight * 0.02,
   },
   rectangleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: windowHeight * 0.02,
     borderWidth: 0.4,
-    borderColor: 'lightgrey', // Bordure noire
+    borderColor: 'lightgrey',
     borderRadius: 10,
-    padding: windowWidth * 0.03
+    padding: windowWidth * 0.03,
   },
   smallImage: {
     width: windowWidth * 0.1,
     height: windowWidth * 0.1,
     resizeMode: 'cover',
-    borderRadius: windowHeight * 0.01,
+    borderRadius: windowWidth * 0.05,
     marginRight: windowWidth * 0.02,
   },
   userInfoContainer: {
-    borderWidth: 1,
-    borderColor: 'black', // Bordure noire
-    marginLeft: windowWidth * 0.02,
-    padding: windowWidth * 0.02,
-    borderRadius: windowHeight * 0.01,
+    flex: 1,
   },
   userInfo: {
-    fontSize: windowHeight * 0.02,
+    fontSize: windowWidth * 0.035,
     fontWeight: 'bold',
   },
   userInfoDescription: {
-    fontSize: windowHeight * 0.018,
+    fontSize: windowWidth * 0.03,
     color: 'gray',
   },
   buttonContainer: {
@@ -160,18 +153,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#F2E8CF',
     flex: 1,
     paddingVertical: windowHeight * 0.03,
-    paddingHorizontal: windowWidth * 0.05,
     marginHorizontal: windowWidth * 0.02,
-    borderRadius: windowHeight * 0.017,
-    alignItems: 'center', // Aligner le contenu au centre
-    justifyContent: 'center', // Aligner le contenu au centre
+    borderRadius: windowWidth * 0.05,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   smallerButton: {
-    flex: 0.5, // Utilisez un flex plus petit pour le bouton "Appeler"
+    flex: 0.5,
   },
   buttonText: {
-    textAlign: 'center',
-    fontSize: windowHeight * 0.02,
+    fontSize: windowWidth * 0.035,
     fontWeight: 'bold',
   },
 });
+
