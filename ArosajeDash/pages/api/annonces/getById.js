60d49f043d11137b3e3c42eab702d1b1f3cd5ab0
Florian <flo.guiot@hotmail.fr>
2024-02-29 12:02:50 +0100

    ville

--- ArosajeDash/pages/api/annonces/getById.js
+++ ArosajeDash/pages/api/annonces/getById.js
@@ -42,6 +42,7 @@ export default async function handler(req, res) {
               prenom: true,
               nom: true,
               cheminPhoto: true,
+              ville: true,
             },
           },
           plante: {
