93a948e8428e1f3c5c885ad5ba1fdd8d435613e5
FlorianGuiot <flo.guiot@hotmail.fr>
2024-03-03 15:07:14 +0100

    test

--- ArosajeDash/pages/api/annonces/getAll.js
+++ ArosajeDash/pages/api/annonces/getAll.js
@@ -29,140 +29,138 @@ export default async function handler(req, res) {
   const token = req.headers.authorization?.split(' ')[1];
 
   // Vérifier la validité du token
-  const decoded = await new Promise((resolve, reject) => {
-    jwt.verify(token, 'secret_key', (err, decoded) => {
+
+   // Vérifier la validité du token
+    jwt.verify(token, 'secret_key', async (err, decoded) => {
       if (err) {
         console.error('Erreur lors de la vérification du token :', err);
-        return res.status(401).end(); // Token non valide
+        return res.status(401).json({ error: 'Token non valide' });
       }
-      resolve(decoded);
-    });
-  });
-
-    const { recherche,rayon,categorie,cp } = req.query;
+      
+      const { recherche,rayon,categorie,cp } = req.query;
 
 
-    try {
+      try {
 
-      var whereCondition = {}
+        var whereCondition = {}
 
-      if(recherche){
-        // Diviser la recherche en mots
-        const motsRecherche = recherche.split(' ');
+        if(recherche){
+          // Diviser la recherche en mots
+          const motsRecherche = recherche.split(' ');
 
-        // Récupérer toutes les annonces où le libellé de la plante ou de la catégorie ressemble à l'un des mots de la recherche
-        whereCondition = {
-            OR: motsRecherche.map((mot) => ({
-            OR: [
-                {
-                plante: {
-                    libelle: {
-                    contains: mot,
-                    },
-                },
-                },
-                {
-                plante: {
-                    categorie: {
-                    libelle: {
-                        contains: mot,
-                    },
-                    },
-                },
-                },
-            ],
-            })),
-        };
-      }
+          // Récupérer toutes les annonces où le libellé de la plante ou de la catégorie ressemble à l'un des mots de la recherche
+          whereCondition = {
+              OR: motsRecherche.map((mot) => ({
+              OR: [
+                  {
+                  plante: {
+                      libelle: {
+                      contains: mot,
+                      },
+                  },
+                  },
+                  {
+                  plante: {
+                      categorie: {
+                      libelle: {
+                          contains: mot,
+                      },
+                      },
+                  },
+                  },
+              ],
+              })),
+          };
+        }
 
-      //Ajoute la condition de categorie si le paramètre est fourni
-      if(categorie){
-        whereCondition.plante = {
-            categorieId: parseInt(categorie, 10),
-          }
-      }
-
-      // Ajouter la condition pour le rayon si le paramètre est fourni
-      if (rayon) {
-        const userId = decoded.userId;
-        var leCodePostal = "";
-        //S'il n'y a pas de code postal, utiliser celui de l'utilisateur connecté
-        if(!cp){
-            const utilisateur = await prisma.utilisateur.findUnique({
-                where: {
-                id : userId,
-                },
-                select: {
-                ville: true,
-                cp: true,
-                rue: true,
-                },
-            });
-            leCodePostal = utilisateur.cp;
-        }else{
-            if (!(await Security.isAdmin(userId))) {
-                return res.status(403).json({ error: 'Vous n\'êtes pas autorisé à consulter ces annonces' });
+        //Ajoute la condition de categorie si le paramètre est fourni
+        if(categorie){
+          whereCondition.plante = {
+              categorieId: parseInt(categorie, 10),
             }
-            leCodePostal = cp;
-        }
-        
-        // Récupérer la liste des villes dans le rayon donné pour l'utilisateur connecté depuis une autre API
-        const villesProches = await Map.getVillesVoisines(leCodePostal,rayon); 
-
-        if (!villesProches) {
-        return res.status(500).json({ error: 'Erreur lors de la récupération de la liste des villes proches' });
         }
 
-        whereCondition.auteur = {
-          cp: {
-            in: villesProches,
-          },
-        };
-      }
+        // Ajouter la condition pour le rayon si le paramètre est fourni
+        if (rayon) {
+          const userId = decoded.userId;
+          var leCodePostal = "";
+          //S'il n'y a pas de code postal, utiliser celui de l'utilisateur connecté
+          if(!cp){
+              const utilisateur = await prisma.utilisateur.findUnique({
+                  where: {
+                  id : userId,
+                  },
+                  select: {
+                  ville: true,
+                  cp: true,
+                  rue: true,
+                  },
+              });
+              leCodePostal = utilisateur.cp;
+          }else{
+              if (!(await Security.isAdmin(userId))) {
+                  return res.status(403).json({ error: 'Vous n\'êtes pas autorisé à consulter ces annonces' });
+              }
+              leCodePostal = cp;
+          }
+          
+          // Récupérer la liste des villes dans le rayon donné pour l'utilisateur connecté depuis une autre API
+          const villesProches = await Map.getVillesVoisines(leCodePostal,rayon); 
 
-      // Récupérer toutes les annonces avec les conditions définies
-      const annonces = await prisma.annonce.findMany({
-        where: whereCondition,
-        include: {
-          auteur: {
-            select: {
-              id: true,
-              email: true,
-              prenom: true,
-              nom: true,
-              description: true,
-              cheminPhoto: true,
-              ville: true,
+          if (!villesProches) {
+          return res.status(500).json({ error: 'Erreur lors de la récupération de la liste des villes proches' });
+          }
+
+          whereCondition.auteur = {
+            cp: {
+              in: villesProches,
             },
-          },
-          plante: {
-            select: {
-              id: true,
-              libelle: true,
-              description: true,
-              categorie: {
-                select: {
-                  id: true,
-                  libelle: true,
+          };
+        }
+
+        // Récupérer toutes les annonces avec les conditions définies
+        const annonces = await prisma.annonce.findMany({
+          where: whereCondition,
+          include: {
+            auteur: {
+              select: {
+                id: true,
+                email: true,
+                prenom: true,
+                nom: true,
+                description: true,
+                cheminPhoto: true,
+                ville: true,
+              },
+            },
+            plante: {
+              select: {
+                id: true,
+                libelle: true,
+                description: true,
+                categorie: {
+                  select: {
+                    id: true,
+                    libelle: true,
+                  },
                 },
               },
             },
-          },
-          lesPhotos: {
-            select: {
-              id: true,
-              cheminPhoto: true,
+            lesPhotos: {
+              select: {
+                id: true,
+                cheminPhoto: true,
+              },
             },
           },
-        },
-      });
+        });
 
-      res.status(200).json(annonces);
-    } catch (error) {
-      console.error('Erreur lors de la recherche des annonces :', error);
-      res.status(500).json({ error: 'Erreur serveur' });
-    } finally {
-      await prisma.$disconnect();
-    }
-
+        res.status(200).json(annonces);
+      } catch (error) {
+        console.error('Erreur lors de la recherche des annonces :', error);
+        res.status(500).json({ error: 'Erreur serveur' });
+      } finally {
+        await prisma.$disconnect();
+      }
+    });
 }
