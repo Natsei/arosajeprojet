87efcb286a0bead3a08372b2e81541dd6c449226
ipiwixx <antoine13141@gmail.com>
2024-03-11 12:50:52 +0100

    Last commited

--- ArosajeDash/pages/api/token/auth.js
+++ ArosajeDash/pages/api/token/auth.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     // Générer le token JWT
     const token = jwt.sign({ userId: utilisateur.id }, 'secret_key', { expiresIn: '1h' });
 
-    res.status(200).json({ token });
+    res.status(200).json({ token, id: utilisateur.id });
   } catch (error) {
     console.error('Erreur lors de l\'authentification :', error);
     res.status(500).json({ error: 'Erreur serveur' });
